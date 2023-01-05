25ed7791b1a0614f39efb371965529fd3719a6eb
unknown <samipthegreat@gmail.com>
2023-01-05 12:57:28 +0545

    My list section added

--- public/js/owl-carousel.js
+++ public/js/owl-carousel.js
@@ -19,3 +19,31 @@ $('.current-matches').owlCarousel({
     navText: ["","<i class='fa-solid fa-chevron-right'></i>"],
     dots : false
 });
+
+
+// User list
+var margin;
+if(window.visualViewport.width < 1200 && window.visualViewport.width >= 600){
+    margin = 200;
+}else if(window.visualViewport.width >= 1200){
+    margin = 415;
+}
+
+$('.user-list .user').owlCarousel({
+    loop:true,
+    margin:margin,
+    nav:false,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
